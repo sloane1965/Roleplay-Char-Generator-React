eaf091b0a304af7b3ad8e7dd68114e57e2e0730b
Michael Sloane <oldmansloane@gmail.com>
2018-01-17 17:19:32 -0500

    Added component to edit character info

---
+++ src/components/character_bio.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class CharacterBio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Surname:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /><br />
+        </label>
+        <label>
+          Race:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Sex:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /><br />
+        </label>
+        <label>
+          Class:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Subclass:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /><br />
+        </label>
+        <label>
+          Weight:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Age:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /><br />
+        </label>
+        <label>
+          Backstory:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Hair Color:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /><br />
+        </label>
+        <label>
+          Eye Color:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /><br />
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default CharacterBio;
---
+++ src/components/dice_rolls.js
@@ -0,0 +1,3 @@
+import React, {Component} from 'react';
+
+class DiceRoll extends React {}
